725b226b7f77c206e8401008f42bae278fd542ce
Delorsmolly <delorsm6@gmail.com>
2022-05-11 07:37:56 -0400

    first commit

---
+++ home.php
@@ -0,0 +1,295 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>M'Store|home</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://kit.fontawesome.com/850bd3208d.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- nav bar start -->
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container">
+            <a class="navbar-brand" href="#">M'<span class="color"> Store</span></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav container">
+                    <li class="nav-item">
+                        <a class="nav-link link1 active" aria-current="page" href="#"><span class="color">HOME</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            PAGE
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Services</a></li>
+                            <li><a class="dropdown-item" href="#">Gallery</a></li>
+                            <li><a class="dropdown-item" href="#">Portfolio</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            SHOP
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item title" href="#">Products Details</a></li>
+                            <li><a class="dropdown-item" href="#">Women & Summer</a></li>
+                            <li><a class="dropdown-item" href="#">Accessories</a></li>
+                            <li><a class="dropdown-item" href="#">Bags</a></li>
+                            <li><a class="dropdown-item" href="#">Shoes & Sandal</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            COSTUMER AREA
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Login</a></li>
+                            <li><a class="dropdown-item" href="#">Registratrion</a></li>
+                            <li><a class="dropdown-item" href="#">My Card</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link1 active" aria-current="page" href="#">CONTACT US</a>
+                    </li>
+                    <li class="nav-item flex media">
+                        <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- nav bar end -->
+    <!-- caroussel start -->
+    <div id="carouselExampleCaptions" class="carousel slide flex carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/1.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block first">
+                    <h5>NEW ARRIVALS 2022</h5>
+                    <p>Reduction de 20% sur chaque articles</p>
+                    <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> SHOP NOW</a></button>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/2.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block second">
+                    <h5>NEW ARRIVALS 2022</h5>
+                    <p>Accesoires women style fashion</p>
+                    <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> SHOP NOW</a></button>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="images/3.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block third">
+                    <h5>NEW ARRIVALS 2022</h5>
+                    <p>Accessoires de décoration interieur</p>
+                    <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> SHOP NOW</a></button>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <!-- caroussel end -->
+    <!-- sevices start -->
+    <div class="services">
+        <div class="row">
+            <div class="col-lg-4 " style="background-color: #f0f4fb;">
+                <i class="fa fa-truck" aria-hidden="true"></i>
+                <h5>LIVRAISON ET RETOUR GRATUITS</h5>
+                <p>Livraison gratuite sur toutes les commandes de plus de 99 $</p>
+            </div>
+            <div class="col-lg-4" style="background-color: rgb(237, 20, 91);">
+                <i class="fa fa-star" style="color: #fff;" aria-hidden="true"></i>
+                <h5 style="color: #fff;">GARANTIE DE REMBOURSEMENT</h5>
+                <p style="color: #fff;">Garantie de remboursement à 100 %</p>
+            </div>
+            <div class="col-lg-4" style="background-color: #f0f4fb;">
+                <i class="fa fa-history" aria-hidden="true"></i>
+                <h5>ASSISTANCE EN LIGNE</h5>
+                <p>Livraison gratuite à travers le monde</p>
+            </div>
+        </div>
+    </div>
+    <!-- sevices end -->
+    <!-- new arrivals start -->
+    <div class="newArrivals flex column">
+        <div class="row flex">
+            <div class="col-lg-6 flex">
+                <img src="images/nouveaux/1.png" alt="" width="80%">
+                <div class="description-new flex column">
+                    <h5>NEW ARRIVALS 2022</h5>
+                    <p>Reduction de 20% sur chaque articles</p>
+                    <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> SHOP NOW</a></button>
+                </div>
+            </div>
+            <div class="col-lg-6 flex">
+                <img src="images/nouveaux/2.png" alt="" width="80%">
+                <div class="description-new flex column">
+                    <h5>NEW ARRIVALS 2022</h5>
+                    <p>Reduction de 20% sur chaque articles</p>
+                    <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> SHOP NOW</a></button>
+                </div>
+            </div>
+        </div>
+        <div class="row flex">
+            <div class="col-lg-6 flex">
+                <img src="images/nouveaux/3.png" alt="" width="80%">
+                <div class="description-new flex column">
+                    <h5>NEW ARRIVALS 2022</h5>
+                    <p>Reduction de 20% sur chaque articles</p>
+                    <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> SHOP NOW</a></button>
+                </div>
+            </div>
+            <div class="col-lg-6 flex">
+                <img src="images/nouveaux/2.png" alt="" width="80%">
+                <div class="description-new flex column">
+                    <h5>NEW ARRIVALS 2022</h5>
+                    <p>Reduction de 20% sur chaque articles</p>
+                    <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> SHOP NOW</a></button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- new arrivals end -->
+    <!--title products start -->
+    <div class="products-title flex">
+        <h5>PRODUITS POPULAIRES</h5>
+    </div>
+    <!-- title products end -->
+    <!-- popular product start -->
+    <div class="popular-product container">
+        <div class="row">
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 1</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 2</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 3</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 4</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 5</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 6</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 7</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 8</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="images/popular/7.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Article 9</h5>
+                        <p class="card-text">Prix | 15 000 fcfa</p>
+                        <button><a href=""><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- popular product end -->
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script>
+
+
+    </script>
+</body>
+
+</html>
